6052ee77e745f0c7e09850f81c536704faa5205d
Dhanush Gowda <145486119+Dhanushgowda10@users.noreply.github.com>
2026-01-25 15:06:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,34 @@ resource "aws_security_group" "app_sg" {
   name        = "devops-assessment-sg"
   description = "Allow HTTP, HTTPS and SSH"
 
-  ingress { from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 443; to_port = 443; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 22; to_port = 22; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "web_server" {
   ami                    = "ami-0b6c6ebed2801a5cb" 
   instance_type          = "t3.micro"
