bb94087d95867401f366b212864028614d606de2
Dhanush Gowda <145486119+Dhanushgowda10@users.noreply.github.com>
2026-01-25 18:01:10 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.28.0"
-    }
+  backend "s3" {
+    bucket = "terraform-state123212"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"
