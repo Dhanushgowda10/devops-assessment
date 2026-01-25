23ce86897d94574496807f75e0f6cf64264daa3e
Dhanush Gowda <145486119+Dhanushgowda10@users.noreply.github.com>
2026-01-25 17:37:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "app_sg" {
-  name        = "devops-assessment-sg1"
+  name        = "devops-assessment-sg12"
   description = "Allow HTTP, HTTPS, SSH, and Django API"
 
   # Frontend Port
